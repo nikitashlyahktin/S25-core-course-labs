73d808322c92a5c556d562881a1b5e092f8ae64f
Nikita Shlyakhtin <nikitashlyakhtin@mail.ru>
2025-02-17 21:20:02 +0300

    Lab08 Bonus Task Submission

--- app_python/app.py
+++ app_python/app.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Response
 from datetime import datetime
 import pytz
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST, Counter
 
 app = Flask(__name__)
+
+REQUEST_COUNT = Counter("request_count", "Requests Counter", ["method", "endpoint"])
 
 
 @app.route("/")
 def moscow_time():
+    REQUEST_COUNT.labels(method="GET", endpoint="/").inc()
+
     tz = pytz.timezone("Europe/Moscow")
     current_time = datetime.now(tz)
 
@@ -16,6 +21,11 @@ def moscow_time():
     }
 
     return jsonify(data)
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
 if __name__ == "__main__":
